36882d7ada9596d8759c46abebb527e15efa4599
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2024-02-18 21:23:13 +0000

    Update example1.R

--- example1.R
+++ example1.R
@@ -6,31 +6,26 @@ expectedResult1<-makeExpected(nsim=1000,hypothesis=hypothesis1,
 expectedResult2<-makeExpected(nsim=1000,hypothesis=hypothesis1,doingNull=TRUE,
                               autoShow=TRUE,type="NHSTErrors")
 
-hypothesis2<-getHypothesis("Psych")
-design2<-getDesign("Psych")
-expectedResult3<-makeExpected(nsim=1000,hypothesis=hypothesis2,design=design2,
-                              autoShow=TRUE)
-
-showExpected(expectedResult3,type="NHSTErrors")
-
-evidence<-makeEvidence(shortHand = TRUE)
-
-exploreResult<-makeExplore(1000,type="SampleSize",hypothesis2,design2,evidence,
-                           autoShow = TRUE, Explore_show = "p(sig)")
-
-exploreResult<-makeExplore(1000,type="SampleSize",hypothesis2,design2,evidence,doingNull=TRUE,
-                           autoShow = TRUE, Explore_show = "NHSTErrors")
-
-
-exploreResult<-makeExplore(1000,type="SampleSize",hypothesis = hypothesis1,
+exploreResult3<-makeExplore(1000,type="SampleSize",hypothesis = hypothesis1,
                            autoShow = TRUE, Explore_show="p(sig)")
 
-exploreResult<-makeExplore(1000,type="SampleSize",hypothesis = hypothesis1,doingNull=TRUE,
+exploreResult4<-makeExplore(1000,type="SampleSize",hypothesis = hypothesis1,doingNull=TRUE,
                            autoShow = TRUE, Explore_show="NHSTErrors")
 
-exploreResult<-makeExplore(1000,type="SampleSize",hypothesis = hypothesis2,design=design2,doingNull=TRUE,
+
+hypothesis2<-getHypothesis("Psych")
+design2<-getDesign("Psych")
+evidence2<-makeEvidence(shortHand = TRUE)
+expectedResult5<-makeExpected(nsim=1000,hypothesis=hypothesis2,design=design2,
+                              autoShow=TRUE)
+
+
+exploreResult<-makeExplore(1000,type="SampleSize",hypothesis2,design2,evidence2,
+                           autoShow = TRUE, Explore_show = "p(sig)")
+
+exploreResult<-makeExplore(1000,type="SampleSize",hypothesis2,design2,doingNull=TRUE,
                            autoShow = TRUE, Explore_show="NHSTErrors")
 
-exploreResult<-makeExplore(1000,type="SampleSize",hypothesis = hypothesis2,design=design2,doingNull=TRUE,
+exploreResult<-makeExplore(1000,type="SampleSize",hypothesis2,design2,doingNull=TRUE,
                            min_n=10,max_n=1000,xlog=TRUE,
                            autoShow = TRUE, Explore_show="NHSTErrors")
