d919a75a9775140b66330744be18dfbeb5e14527
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2024-02-19 19:50:24 +0000

    Update showSample.R

--- showSample.R
+++ showSample.R
@@ -1,28 +1,43 @@
+plotSample<-function(IV,DV,effect,ivplot,dvplot) {
+  # the population
+  g<-plotPopulation(IV,DV,effect,alpha=0.75,theme=BrawOpts$plotTheme)
+  
+  # the scattered points
+  dotSize<-(BrawOpts$plotTheme$axis.title$size)/3
+  if (length(ivplot)>100) {
+    dotSize<-dotSize*sqrt(100/length(iv))
+  }
+  
+  x<-ivplot
+  y<-dvplot
+  pts<-data.frame(x=x,y=y);
+  g<-g+geom_point(data=pts,aes(x=x,y=y),shape=BrawOpts$plotShapes$data, colour = "black", fill = BrawOpts$plotColours$sampleC, size = dotSize)
+  if (showMedians) {
+    if (sample$type=="Categorical") {yuse<-0.5} else {yuse<-median(y)}
+    g<-g+geom_hline(yintercept=yuse,col="red")
+    if (sample$type=="Categorical") {xuse<-0.5} else {xuse<-median(x)}
+    g<-g+geom_vline(xintercept=xuse,col="red")
+  }
+  g<-g+labs(x=IV$name,y=DV$name)+BrawOpts$plotTheme
+  g
+  
+}
+
 showSample<-function(sample=makeSample()){
   IV<-sample$hypothesis$IV
   IV2<-sample$hypothesis$IV2
   DV<-sample$hypothesis$DV
   effect<-sample$hypothesis$effect
   
-  # the population
-  g<-plotPopulation(IV,DV,effect,alpha=0.75,theme=BrawOpts$plotTheme)
-  
-  # the scattered points
-  dotSize<-(BrawOpts$plotTheme$axis.title$size)/3
-  if (sample$nval>100) {
-    dotSize<-dotSize*sqrt(100/sample$nval)
+  if (is.null(IV2)) {
+    g<-plotSample(IV,DV,effect,sample$ivplot,sample$dvplot)
+  } else {
+    g1<-plotSample(IV,IV2,effect,sample$ivplot,sample$iv2plot)
+    g2<-plotSample(IV,DV,effect,sample$ivplot,sample$dvplot)
+    g3<-plotSample(IV2,DV,effect,sample$iv2plot,sample$dvplot)
+    g<-joinPlots(g1,g2,g3)
   }
-  x<-sample$ivplot
-  y<-sample$dvplot
-  pts<-data.frame(x=x,y=y);
-  g<-g+geom_point(data=pts,aes(x=x,y=y),shape=BrawOpts$plotShapes$data, colour = "black", fill = BrawOpts$plotColours$sampleC, size = dotSize)
-  if (showMedians) {
-    if (sample$type=="Categorical") {yuse<-0.5} else {yuse<-median(y)}
-      g<-g+geom_hline(yintercept=yuse,col="red")
-      if (sample$type=="Categorical") {xuse<-0.5} else {xuse<-median(x)}
-      g<-g+geom_vline(xintercept=xuse,col="red")
-  }
-  g<-g+labs(x=IV$name,y=DV$name)+BrawOpts$plotTheme
-  g
   
+  
+  return(g)
 }
