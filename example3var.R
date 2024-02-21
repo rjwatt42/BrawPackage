aafdae1cb58cf563acee2a78548b1dd116b2b733
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2024-02-21 21:22:18 +0000

    Update example3var.R

--- example3var.R
+++ example3var.R
@@ -3,6 +3,6 @@
 hypothesis<-makeHypothesis(IV=makeVariable("IV","Categorical"),
                            IV2=makeVariable("IV2","Interval"),
                            DV=makeVariable("DV","Interval"),
-                           effect=makeEffect(0.3,0.,0,0.))
+                           effect=makeEffect(0.3,-0.3,0,0.))
 
 showPrediction(hypothesis)
