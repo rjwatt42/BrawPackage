c803fa9a23401786ed56e90f0cad5ea873fc240c
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2024-03-03 19:41:43 +0000

    more

--- R/packages.R
+++ R/packages.R
@@ -3,28 +3,22 @@
 # source("https://github.com/rjwatt42/BrawStats/raw/main/packages.R")
 # suppressPackageStartupMessages
 
-list.of.packages<-c("ggplot2","gridExtra","tidyr",
+list.of.packages<-c("ggplot2","tidyr",
                     "mnormt","lme4",
-                    "readxl","writexl","car","stringi","stringr","clipr",
-                    "SuppDists","e1071","pracma",
-                    "NlcOptim"
+                    "readxl","writexl","stringr","clipr",
+                    "car","pracma"
 )
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)>0) install.packages(new.packages,repos="http://cran.fhcrc.org")
 
 library(ggplot2)
-library(gridExtra)
 library(tidyr)
 
-# library(gridExtra)   # not sure any longer
 library(mnormt)      # pmnorm for logistic
 library(lme4)        # lmer (mixed models)
 library(readxl)      # excel
 library(writexl)     # x excel
-library(car)         # Anova type 3 correct
 library(stringr)     # for str_* functions
 library(clipr)       # for clipboard functions
-# library(SuppDists)   # for Johnson distributions
-library(e1071)       # for skewness and kurtosis
+library(car)         # Anova type 3 correct
 library(pracma)      # for meshgrid & fmincon
-library(NlcOptim)    # for fmincon
