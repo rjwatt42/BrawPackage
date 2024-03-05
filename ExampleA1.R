ec0448d9c9b672c3c3e5e2960012fe5dafc6c5d4
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2024-03-05 22:03:38 +0000

    more

--- ExampleA1.R
+++ ExampleA1.R
@@ -8,8 +8,11 @@ explore<-makeExplore(nsims=100,NULL,"Usage",hypothesis=hypothesis,design=design)
 explore<-makeExplore(nsims=10,NULL,"n")
 
 hypothesis=getHypothesis("Psych")
+hypothesis$effect$world$populationNullp<-0
 explore<-makeExplore(nsims=1000,NULL,"n",max_n=1000,xlog=TRUE,hypothesis=hypothesis)
 
+expect<-makeExpected(nsims=100000,hypothesis=hypothesis,evidence=makeEvidence(shortHand=TRUE))
+expect<-makeExpected(nsims=100000,hypothesis=makeHypothesis(effect=makeEffect(rIV=0.3)),evidence=makeEvidence(shortHand=TRUE))
 
 
 expect<-makeExpected(nsims=1000,doingNull=TRUE)
