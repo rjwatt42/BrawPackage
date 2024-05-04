3328c903fe0e72e3447ceb9b37e0bb32b6f43a18
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2024-05-04 13:49:32 +0100

    more

--- R/plotAxis.R
+++ R/plotAxis.R
@@ -238,6 +238,12 @@ plotAxis<-function(showType,effect) {
             ylabel<-"False Miss"
             use_cols<-braw.env$plotColours$fmr
           },
+          "Lambda"={
+            ylim<-c(-0.01,1.01)
+            ytick<-seq(0,1,0.2)
+            ymins<-seq(0,1,0.1)
+            ylabel<-braw.env$Llabel
+          },
           "k"={
             ylim<-c(-0.01,1.01)
             ytick<-seq(0,1,0.2)
