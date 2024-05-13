336363ecee6b3300a5364db6ed573a1cd19b8c94
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2024-05-13 22:07:00 +0100

    bug fixes

--- example3.R
+++ example3.R
@@ -1,47 +1,8 @@
-###########################################
-#
 
-source("StartUp.R")
+hypothesis<-getHypothesis("3")
+hypothesis$effect$rIV<-0.3
+hypothesis$effect$rIV2<-0.5
 
-###########################################
-#
 
-hypothesis1<-makeHypothesis(IV=makeVariable("IV","Categorical"),effect=makeEffect(rIV=0.3))
-design1<-makeDesign(sIV1Use="Between")
-design2<-makeDesign(sIV1Use="Within")
+showDescription(doAnalysis(doSample(hypothesis=hypothesis)))
 
-sample<-doSample(hypothesis1,design1)
-showSample(sample)
-analysis<-doAnalysis(sample=sample)
-showDescription(analysis)
-
-###########################################
-#
-
-expectedResult1<-doExpected(nsim=1000,hypothesis=hypothesis1,
-                              autoShow=TRUE)
-
-expectedResult2<-doExpected(nsim=1000,hypothesis=hypothesis1,doingNull=TRUE,
-                              autoShow=TRUE,showType="NHST")
-
-expectedResult3<-doExpected(nsim=1000,hypothesis=hypothesis2,design=design2,
-                              autoShow=TRUE)
-
-###########################################
-#
-
-exploreResult1<-doExplore(1000,exploreType="n",hypothesis = hypothesis1,
-                            autoShow = TRUE, showType="p(sig)")
-
-exploreResult2<-doExplore(1000,exploreType="n",hypothesis = hypothesis1,doingNull=TRUE,
-                            autoShow = TRUE, showType="NHST")
-
-exploreResult4<-doExplore(1000,exploreType="n",hypothesis2,design2,evidence2,
-                           autoShow = TRUE, showType = "p(sig)")
-
-exploreResult5<-doExplore(1000,exploreType="n",hypothesis2,design2,doingNull=TRUE,
-                           autoShow = TRUE, showType="NHST")
-
-exploreResult6<-doExplore(1000,exploreType="n",hypothesis2,design2,doingNull=TRUE,
-                           min_n=10,max_n=1000,xlog=TRUE,
-                           autoShow = TRUE, showType="NHST")
