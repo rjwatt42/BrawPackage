d2f4981489cbf0b4c865adddd744726d01dd96d6
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2025-06-21 17:37:48 +0100

    Update doAnalysis.R

--- R/doAnalysis.R
+++ R/doAnalysis.R
@@ -541,7 +541,7 @@ generalAnalysis<-function(allData,InteractionOn,withins=FALSE,ssqType="Type3",ca
   llk<-logLik(lmNormC)
 
   k<-2
-  n_obs<-n
+  n_obs<-1
   n_data<-n
   resid2<-sum((analysisRawData$dv-mean(analysisRawData$dv))^2)
   aicNull<-2*k+n_obs*(log(2*pi*resid2/n_data)+1)
