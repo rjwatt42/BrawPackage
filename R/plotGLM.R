db9c07d465da82920006bb1f544a55852ac436a1
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2025-06-21 11:01:29 +0100

    Update plotGLM.R

--- R/plotGLM.R
+++ R/plotGLM.R
@@ -30,7 +30,8 @@ plotGLM<-function(DV,IVs,result,whichR) {
   g<-startPlot(xlim=xlim,ylim=ylim,box="none",g=NULL)
   # g<-addG(g,dataPolygon(data.frame(x=c(-1,-1,1,1)*14,y=c(-1,1,1,-1)*9),col=braw.env$plotColours$graphBack,fill=braw.env$plotColours$graphBack))
   
-  g<-addG(g,dataLabel(data.frame(x=0,y=0),label=DV$name,hjust=0,vjust=0.5,fontface="bold",size=1.25))
+  fill<-"#FFAAAA"
+  g<-addG(g,dataLabel(data.frame(x=0,y=0),label=DV$name,hjust=0,vjust=0.5,fontface="bold",size=1,fill=fill))
   xStart<-4+nchar(DV$name)/2*(fontSize/14)
   arrowLength<-4-1
   yRange<-max(sum(r>0),sum(r<0))/2
@@ -48,12 +49,7 @@ plotGLM<-function(DV,IVs,result,whichR) {
       if (r1<0) colArrow<-"#0088FF"
       else      colArrow="#FFEE00"
       colLabel<-colArrow
-      if (p[use[i]]<braw.env$alphaSig) {
-        labelFill<-colArrow
-      } else {
-        labelFill<-braw.env$plotColours$graphBack
-      }
-      
+
       if (abs(r1)<0.1) {
         colArrow<-desat(colArrow,0.1)
         colLine<-desat(colLine,0.1)
@@ -71,10 +67,13 @@ plotGLM<-function(DV,IVs,result,whichR) {
       labelWidth<-arrowWidth*4
       arrowWidth<-arrowWidth*1.6
       # colArrow<-desat(colArrow,gain=abs(r[use[i]])^0.5)
+      fill<-"#CCFF44"
       g<-addG(g,dataLabel(data.frame(x=-xStart,y=y[i]),label=IVs$name[use[i]],hjust=1,vjust=0.5,
-                          col="#000000",fill=labelFill,size=1.25,label.size=labelWidth))
+                          col="#000000",fill=fill,size=1,label.size=labelWidth))
       g<-addG(g,drawArrow(start=c(-xStart,y[i]),arrowLength,direction=90+direction,ends="last",finAngle=60,
                           col=colLine,fill=colArrow,width=arrowWidth))
+      g<-addG(g,dataLabel(data.frame(x=-xStart/2,y=y[i]/2),label=brawFormat(r1,digits=2),hjust=0.5,vjust=0.5,
+                          col=colLine,fill=colArrow,size=0.75))
     }
 
     g<-addG(g,dataText(data.frame(x=xlim[1],y=ylim[1]),label=paste0("r[model]=",brawFormat(result$r.full,3)),vjust=0))
