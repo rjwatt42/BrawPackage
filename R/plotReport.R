fa85b7eaee258990dc6313986b5cce56f8b9b142
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2025-06-22 13:05:29 +0100

    Update plotReport.R

--- R/plotReport.R
+++ R/plotReport.R
@@ -73,6 +73,7 @@ reportPlot<-function(outputText,nc,nr,fontSize=0.85,maxRows=14,renderAsHTML=braw
           col1Style<-paste0(col1Style,"font-weight:bold;")
           col1Style<-paste0(col1Style,"min-width:",indentSize,";")
           col1Style<-paste0(col1Style,"text-align:right;padding-right:5px;")
+          col1Style<-paste0(col1Style,"right-bottom:solid;right-bottom-color:",lineColour,";")
           outputText[index+(1:nc)]<-sub("!C","",outputText[index+(1:nc)])
         }
         if (any(grepl("!D",outputText[index+(1:nc)]))) {
