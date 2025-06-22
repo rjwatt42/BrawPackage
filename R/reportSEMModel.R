2ba3110b9adae1a0ad63c11ff43cb3a738a00915
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2025-06-22 05:24:48 +0100

    Update reportSEMModel.R

--- R/reportSEMModel.R
+++ R/reportSEMModel.R
@@ -100,7 +100,7 @@ reportSEMModel<-function(sem,showType) {
   columns<-c("Model","AIC","AICnull","Rsqr","r","llr","k","n","obs")
   nc1<-length(columns)
   tableText<-c("!TStatistics",rep("",nc-1),columns,rep("",nc-nc1))
-  tableText[1]<-paste0("!H",tableText[1])
+  tableText[1]<-paste0("!H",tableText[nc+1])
   tableText[which(columns=="Rsqr")]<-"R^2"
   digitsE<-c(0,1,1,1,1,3,3,3,1,0,0,0)
   prefix<-"!r"
