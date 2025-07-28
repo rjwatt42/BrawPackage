56ae610f8b3efefc8f7398fd89c86e04513319a7
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2025-07-28 12:21:24 +0100

    Update plotInference.R

--- R/plotInference.R
+++ R/plotInference.R
@@ -292,18 +292,22 @@ plot2Inference<-function(analysis,disp1,disp2,metaPlot=FALSE,sequence=FALSE){
     use<-rep(FALSE,length(d1))
     shape<-braw.env$plotShapes$meta
   }
-  pts1=pts[use,]
   if (sequence) dotSize<-dotSize*0.5
   if (length(d1)>100) {b1<-c1;b2<-c2} else {b1<-b2<-"#000000"}
-  g<-addG(g,dataPoint(data=pts1,shape=shape, colour = b2, fill = c2, alpha=gain^0.8, size = dotSize))
-  pts1$x<-pts1$x+diff(xaxis$lim)*0.025
-  if (sequence)
-    g<-addG(g,dataLabel(data=pts1,labels[use],vjust=0.5,size=0.75))
-  pts2=pts[!use,]
+  if (any(use)) {
+    pts1=pts[use,]
+    g<-addG(g,dataPoint(data=pts1,shape=shape, colour = b2, fill = c2, alpha=gain^0.8, size = dotSize))
+    pts1$x<-pts1$x+diff(xaxis$lim)*0.025
+    if (sequence)
+      g<-addG(g,dataLabel(data=pts1,labels[use],vjust=0.5,size=0.75))
+  }
+  if (any(!use)) {
+    pts2=pts[!use,]
   g<-addG(g,dataPoint(data=pts2,shape=shape, colour = b1, fill = c1, alpha=gain^0.8, size = dotSize))
   pts2$x<-pts2$x+diff(xaxis$lim)*0.025
   if (sequence)
     g<-addG(g,dataLabel(data=pts2,labels[!use],vjust=0.5,size=0.75))
+  }
   
   if (sequence) {
     last<-length(pts$x)
