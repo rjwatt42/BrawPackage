35b899e7c325657814cdfc8fac57ee1968dcb095
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2025-08-04 09:22:24 +0100

    Update reportSystem.R

--- R/reportSystem.R
+++ R/reportSystem.R
@@ -145,11 +145,11 @@ reportDesign<-function(design=braw.def$design,plain=FALSE) {
   if (design$sCheating!="None") {
     if (design$sCheating=="Retry")    
       outputText<-c(outputText,rep("",4),
-                    paste0("!j",brawFormat(design$sCheatingAttempts,digits=2)),
+                    paste0("!j","Attempts","=",brawFormat(design$sCheatingAttempts,digits=2)),
                     rep("",nc-5))
     else
       outputText<-c(outputText,rep("",4),
-                    paste0("!j",brawFormat(design$sCheatingBudget,digits=2)),
+                    paste0("!j",tolower(design$sCheatingLimit),"=",brawFormat(design$sCheatingBudget,digits=2)),
                     rep("",nc-5))
   }
   
